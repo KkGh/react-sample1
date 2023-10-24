e7a8c278e94e17c203764a2074a757dc02fa6766
kos <geneofgene@gmail.com>
2023-10-24 23:58:06 +0900

    add roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,124 @@
+import DraftsIcon from "@mui/icons-material/Drafts";
+import InboxIcon from "@mui/icons-material/Inbox";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import { ChangeEvent, useCallback, useState } from "react";
-import Textbox from "./components/Textbox";
 
 const App = () => {
   const [text, setText] = useState("sample");
-  const [value, setValue] = useState(10);
+  const [value, setValue] = useState(5);
 
-  const handleTextChange = useCallback((text: string) => {
-    setText(text);
-  }, []);
+  const handleTextChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setText(event.target.value);
+    },
+    []
+  );
 
-  const handleValueChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    return setValue(e.target.value as unknown as number);
-  }, []);
+  const handleValueChange = useCallback(
+    (event: Event, value: number | number[], activeThumb: number) => {
+      setValue(value as number);
+    },
+    []
+  );
 
   return (
-    <div>
-      {<Textbox text={text} onChange={handleTextChange} />}
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        React sample
+      </Typography>
 
-      <div>
-        repeated
-        <input type="number" value={value} onChange={handleValueChange} />
-        times:
-      </div>
-      <textarea readOnly rows={20} cols={40} value={text.repeat(value)} />
-    </div>
+      <TextField fullWidth value={text} onChange={handleTextChange} />
+      <Slider value={value} onChange={handleValueChange} />
+
+      <Box mb={2}>
+        repeated {value} times:
+        <Box>{text.repeat(value)}</Box>
+      </Box>
+
+      <List>
+        <ListItemButton>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItemButton>
+      </List>
+
+      <Box mb={2}>
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Header 1</TableCell>
+                <TableCell>Header 2</TableCell>
+                <TableCell>Header 3</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Row 1, Cell 1</TableCell>
+                <TableCell>Row 1, Cell 2</TableCell>
+                <TableCell>Row 1, Cell 3</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Row 2, Cell 1</TableCell>
+                <TableCell>Row 2, Cell 2</TableCell>
+                <TableCell>Row 2, Cell 3</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+
+      <Grid
+        mb={2}
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        <Grid item xs={6}>
+          <Paper>Item 1</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>Item 2</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>Item 3</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>Item 4</Paper>
+        </Grid>
+      </Grid>
+
+      <Stack mb={2} direction="row" spacing={2}>
+        <Paper elevation={1}>Item 1</Paper>
+        <Paper elevation={3}>Item 2</Paper>
+        <Paper elevation={5}>Item 3</Paper>
+      </Stack>
+    </Container>
   );
 };
 
