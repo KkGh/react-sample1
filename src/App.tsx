e23eeedaeab7b590cd099d9fce2c241908641f63
kos <geneofgene@gmail.com>
2023-10-24 13:34:47 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { useCallback, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 import Textbox from "./components/Textbox";
 
 const App = () => {
   const [text, setText] = useState("sample");
   const [value, setValue] = useState(10);
 
-
-  const handleChange = useCallback((text: string) => {
+  const handleTextChange = useCallback((text: string) => {
     setText(text);
+  }, []);
+
+  const handleValueChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    return setValue(e.target.value as unknown as number);
   }, []);
 
   return (
     <div>
-      {<Textbox text={text} onChange={handleChange} />}
+      {<Textbox text={text} onChange={handleTextChange} />}
 
-      <input type="number" value={value} onChange={e => setValue(e.target.value as unknown as number)} />
-      <div>great processing...</div>
-      <div>powerful processing...</div>
+      <div>
+        <input type="number" value={value} onChange={handleValueChange} />
+      </div>
+
       <div>result: {text.repeat(value)}</div>
     </div>
   );
